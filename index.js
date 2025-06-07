c18a43fc06dffbeffc3019528e9402f4382a019b
Deylin <deylinbo91@gmail.com>
2025-06-06 21:42:03 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ import fs, {readdirSync, statSync, unlinkSync, existsSync, mkdirSync, readFileSy
 import yargs from 'yargs';
 import {spawn} from 'child_process'
 import lodash from 'lodash'
-import pkg from './plugins/jadibot-serbot.js';
-const { kiritoJadiBot } = pkg;
+import { kiritoJadiBot } from './plugins/jadibot-serbot.js';
 import chalk from 'chalk'
 import syntaxerror from 'syntax-error'
 import {tmpdir} from 'os'
