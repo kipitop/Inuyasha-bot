a790cf72977f7f78630c07f11f9541b43997772a
Deylin <deylinbo91@gmail.com>
2025-06-08 22:17:01 -0600

    Actualizar owner-resetprefix.js

--- plugins/owner-resetprefix.js
+++ plugins/owner-resetprefix.js
@@ -1,11 +1,15 @@
 const handler = async (m, {conn}) => {
+  
+    if (!text.trim()) {
+      return conn.reply(m.chat, `*Ejemplo:* resetprefix ${prefix}`, m, rcanal);
+    }
   global.prefix = new RegExp('^[' + (opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
   //await m.reply(`✅️ *Prefijo Restablecido Con Éxito!*`);
   conn.fakeReply(m.chat, `${emoji} *Prefijo Restablecido Con Éxito!*`, '0@s.whatsapp.net', '✨ PREFIJO RESTABLECIDO ✨')
 };
 handler.help = ['resetprefix'];
-handler.customPrefix = /^(resetprefix)$/i
-handler.command = new RegExp
+handler.tags = ['owner'];
+handler.command = ['resetprefix'];
 handler.rowner = true;
 
 export default handler;
