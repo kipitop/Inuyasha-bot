864837ba66e3cdd4f29e7c66c95f15ea80988927
Deylin <deylinbo91@gmail.com>
2025-06-08 21:35:46 -0600

    Actualizar owner-prefix.js

--- plugins/owner-prefix.js
+++ plugins/owner-prefix.js
@@ -1,6 +1,6 @@
 const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (!text) throw `${emoji} No Se Encontró Ningun Prefijo, Por Favor Escriba Un Prefijo.\n> *Ejemplo: ${usedPrefix + command} !*`;
-  global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || °•∆π÷×¶√✓™©®¥€£¢₩$#@!?¿=+~^*_&|<>%:;(){}[]🔥💥✨⚡👑🤖🐱🧠🎩🎮🌟🌀🎯💫🎆🎇💎🚀🛸🧩🎉🎊🕹️🧿🥷🦾🪄💣📦📌📍🔮🧨🪐🥇🥈🥉🏆🎖️🏅🎗️💡🧱🚧📢🔔🔊🎵🎶💀👻☠️👽👾🦴🦾🧬🧪🕳️⚔️🛡️🏹🗡️🔫🚬💊🔋🔌🧲📡📶🧠🔧🛠️🗜️⚙️🪛🧰🗂️📁📂📜📝📖📚📎🖇️📐📏🧮📊📈📉🗃️🗄️🔍🔎💼🎒🧳🚪🪑🛏️').replace(°•∆π÷×¶√✓™©®¥€£¢₩$#@!?¿=+~^*_&|<>%:;(){}[]🔥💥✨⚡👑🤖🐱🧠🎩🎮🌟🌀🎯💫🎆🎇💎🚀🛸🧩🎉🎊🕹️🧿🥷🦾🪄💣📦📌📍🔮🧨🪐🥇🥈🥉🏆🎖️🏅🎗️💡🧱🚧📢🔔🔊🎵🎶💀👻☠️👽👾🦴🦾🧬🧪🕳️⚔️🛡️🏹🗡️🔫🚬💊🔋🔌🧲📡📶🧠🔧🛠️🗜️⚙️🪛🧰🗂️📁📂📜📝📖📚📎🖇️📐📏🧮📊📈📉🗃️🗄️🔍🔎💼🎒🧳🚪🪑🛏️);
+  global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || °•∆π÷×¶√✓™©®¥€£¢₩$#@!?¿=+~^*_&).replace(°•∆π÷×¶√✓™©®¥€£¢₩$#@!?¿=+~^*_&|<>%:;(){}[]🔥💥✨⚡👑🤖🐱🧠🎩🎮🌟🌀🎯💫🎆🎇💎🚀🛸🧩🎉🎊🕹️🧿🥷🦾🪄💣📦📌📍🔮🧨🪐🥇🥈🥉🏆🎖️🏅🎗️💡🧱🚧📢🔔🔊🎵🎶💀👻☠️👽👾🦴🦾🧬🧪🕳️⚔️🛡️🏹🗡️🔫🚬💊🔋🔌🧲📡📶🧠🔧🛠️🗜️⚙️🪛🧰🗂️📁📂📜📝📖📚📎🖇️📐📏🧮📊📈📉🗃️🗄️🔍🔎💼🎒🧳🚪🪑🛏️);
  // await m.reply(`*✅️ Prefijo Actualizado Con Éxito, Prefijo Actual: ${text}*`);
   conn.fakeReply(m.chat, `${done} *Prefijo Actualizado Con Éxito, Prefijo Actual: ${text}*`, '0@s.whatsapp.net', '✨ PREFIJO NUEVO ✨')
 };
