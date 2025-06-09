4b77c93d980275ea52fa626c1d52018947d55553
Deylin <deylinbo91@gmail.com>
2025-06-08 21:21:23 -0600

    Actualizar owner-prefix.js

--- plugins/owner-prefix.js
+++ plugins/owner-prefix.js
@@ -1,6 +1,6 @@
 const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (!text) throw `${emoji} No Se Encontró Ningun Prefijo, Por Favor Escriba Un Prefijo.\n> *Ejemplo: ${usedPrefix + command} !*`;
-  global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&'⚡) + ']');
+  global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎${prefijo}').replace(`${prefijo}`);
  // await m.reply(`*✅️ Prefijo Actualizado Con Éxito, Prefijo Actual: ${text}*`);
   conn.fakeReply(m.chat, `${done} *Prefijo Actualizado Con Éxito, Prefijo Actual: ${text}*`, '0@s.whatsapp.net', '✨ PREFIJO NUEVO ✨')
 };
