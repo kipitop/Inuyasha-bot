3b5ed0214279f0db470c130120bf86424ec06b69
Deylin <deylinbo91@gmail.com>
2025-06-08 20:11:35 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -299,8 +299,8 @@ if (!opts['restrict'])
 if (plugin.tags && plugin.tags.includes('admin')) {
 continue
 }
-const str2Regex = str => str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&' )
-const prefx = str => str.replace('')
+const str2Regex = str => str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&'')
+//const prefx = str => str.replace('')
 let _prefix = plugin.customPrefix ? plugin.customPrefix : conn.prefix ? conn.prefix : global.prefix
 let match = (_prefix instanceof RegExp ? 
 [[_prefix.exec(m.text), _prefix]] :
