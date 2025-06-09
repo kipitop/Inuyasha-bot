69aaaf4e969f3dad3e4155bdd2d7a79f3a16aa3d
Deylin <deylinbo91@gmail.com>
2025-06-08 22:03:44 -0600

    Actualizar owner-prefix.js

--- plugins/owner-prefix.js
+++ plugins/owner-prefix.js
@@ -1,7 +1,11 @@
 const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (!text) throw `${emoji} No Se Encontró Ningun Prefijo, Por Favor Escriba Un Prefijo.\n> *Ejemplo: ${usedPrefix + command} !*`;
-  global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&'🔥💥✨⚡👑🤖🐱🧠🎩🎮🌟🌀🎯💫🎆🎇💎🚀🛸🧩🎉🎊🕹️🧿🥷🦾🪄💣📦📌📍🔮🧨🪐🥇🥈🥉🏆🎖️🏅🎗️💡🧱🚧📢🔔🔊🎵🎶💀👻☠️👽👾🦴🦾🧬🧪🕳️⚔️🛡️🏹🗡️🔫🚬💊🔋🔌🧲📡📶🧠🔧🛠️🗜️⚙️🪛🧰🗂️📁📂📜📝📖📚📎🖇️📐📏🧮📊📈📉🗃️🗄️🔍🔎💼🎒🧳🚪🪑🛏️) + ']');
+  global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^⚡👑]/g, '\\$&') + ']');
  // await m.reply(`*✅️ Prefijo Actualizado Con Éxito, Prefijo Actual: ${text}*`);
+  try {
+    if (!text.trim()) {
+      return conn.reply(m.chat, "Ejempl: #prefix ${prefix = 1}", m, rcanal);
+    }
   conn.fakeReply(m.chat, `${done} *Prefijo Actualizado Con Éxito, Prefijo Actual: ${text}*`, '0@s.whatsapp.net', '✨ PREFIJO NUEVO ✨')
 };
 handler.help = ['prefix'];
