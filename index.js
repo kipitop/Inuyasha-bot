e396fc62d6a925aa9ed0ced4d51b3ff430d44107
Deylin <deylinbo91@gmail.com>
2025-06-09 19:51:50 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ return msg?.message || ""
 msgRetryCounterCache,
 msgRetryCounterMap,
 defaultQueryTimeoutMs: undefined,
-version,
+version: [2, 3000, 1023223821],
 }
 
 global.conn = makeWASocket(connectionOptions);
