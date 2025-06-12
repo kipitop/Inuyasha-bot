7dc305b4c20d5f5b7890524f001ee3cd9cc00e3a
Deylin <deylinbo91@gmail.com>
2025-06-11 23:19:35 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -268,7 +268,10 @@ let usedPrefix
 
 const groupMetadata = (m.isGroup ? ((conn.chats[m.chat] || {}).metadata || await this.groupMetadata(m.chat).catch(_ => null)) : {}) || {}
 const participants = (m.isGroup ? groupMetadata.participants : []) || []
-let numBot = conn.user.lid.replace(/:.*/, '') || false
+let numBot = false
+if (conn.user && conn.user.lid) {
+  numBot = conn.user.lid.replace(/:.*/, '')
+}
 const detectwhat2 = m.sender.includes('@lid') ? `${numBot}@lid` : conn.user.jid
 const user = (m.isGroup ? participants.find(u => conn.decodeJid(u.id) === m.sender) : {}) || {}
 const bot = (m.isGroup ? participants.find(u => conn.decodeJid(u.id) == detectwhat2) : {}) || {}
