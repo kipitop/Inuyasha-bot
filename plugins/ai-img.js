e2203f54df7fb8fee0e2ea6d6c2d9689e2109100
Deylin <deylinbo91@gmail.com>
2025-07-12 10:29:07 -0600

    Actualizar ai-img.js

--- plugins/ai-img.js
+++ plugins/ai-img.js
@@ -5,10 +5,10 @@ https://github.com/deylin-eliac
 
 let handler = async (m, { text, conn }) => {
   if (!text) {
-    return await conn.reply(m.chat, `${emoji} Escribe el prompt de la imagen. Ejemplo:\n.imagina un dragón azul volando en el espacio`, m)
+    return await conn.reply(m.chat, `${emoji} Escribe el prompt de la imagen. Ejemplo:\n.imagina un dragón azul volando en el espacio`, m, fake)
   }
 
-  await conn.reply(m.chat, `${emoji} Generando imagen de: "${text}", espera un momento...`, m)
+  await conn.reply(m.chat, `${emoji} Generando imagen de: "${text}", \n}nespera un momento...`, m, fake)
 
   try {
     const prompt = encodeURIComponent(text.trim())
@@ -22,7 +22,8 @@ let handler = async (m, { text, conn }) => {
       imageUrl,
       'imagen.jpg',
       `${emoji}\n\n🧃 Imagen generada:\n${imageUrl}`,
-      m
+      m,
+      fake
     )
   } catch (e) {
     console.error(e)
