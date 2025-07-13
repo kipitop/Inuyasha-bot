7009495a282597901f1baed96c1bac4edfefe4a9
Deylin <deylinbo91@gmail.com>
2025-07-13 16:54:48 -0600

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,26 +1,38 @@
-import fetch from 'node-fetch'
-
 let handler = async (m, { conn }) => {
+  const grupoOficial = 'https://chat.whatsapp.com/GPbAHdod8e1AxZYmZHdFSI';
+  const canalOficial = channel;
+  const imagen = 'https://raw.githubusercontent.com/Deylin-Eliac/kirito-bot-MD/main/src/catalogo.jpg';
 
-let grupos = `╭━━《 *GRUPOS OFICIALES* 》━━╮
+  await conn.sendMessage(m.chat, {
+    image: { url: imagen },
+    caption: `🌐 *ÚNETE A NUESTROS ESPACIOS OFICIALES:*
 
-✧ *Únete a nuestro grupo:*  
-➥ *${namegrupo}*  
-⌁ ${gp1}  
+👥 *Grupo oficial de Kirito-Bot*
+${grupoOficial}
 
-━═━═━═━═━═━═━═━═━═━═━
+📢 *Canal de novedades*
+${canalOficial}
 
-⚠ *¿Enlace inválido? Usa el alternativo:*  
-➥ *${namechannel}*  
-⌁ ${channel}  
-
-━═━═━═━═━═━═━═━═━═━═━  
-> ${dev}`
-
-await conn.sendFile(m.chat, catalogo, "grupos.jpg", grupos, m)
-await m.react(emojis)
-
-}
+👑 *By Deylin - Kirito-Bot MD*`,
+    contextInfo: {
+      externalAdReply: {
+        showAdAttribution: true,
+        title: 'Únete al grupo oficial del bot',
+        body: 'Comunidad, noticias y comandos nuevos',
+        thumbnailUrl: imagen,
+        sourceUrl: grupoOficial,
+        mediaType: 2,
+        renderLargerThumbnail: true
+      },
+      
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: '120363302872055226@newsletter.whatsapp.net', 
+        newsletterName: 'Kirito-Bot MD Channel', 
+        serverMessageId: '' 
+      }
+    }
+  }, { quoted: m });
+};
 
 handler.help = ['grupos']
 handler.tags = ['info']
