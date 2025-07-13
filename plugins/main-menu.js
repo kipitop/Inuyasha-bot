9e010055c7a16cdb16fbfb77571a44c393daf2b7
Deylin <deylinbo91@gmail.com>
2025-07-13 16:10:08 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -100,12 +100,10 @@ ${commandsForTag.map(menu => menu.help.map(help =>
   'https://raw.githubusercontent.com/Deylin-Eliac/kirito-bot-MD/main/src/catalogo.jpg',
   'https://raw.githubusercontent.com/Deylin-Eliac/kirito-bot-MD/main/src/catalogo.jpg'
 ];
-const selectedImage = imageUrls[Math.floor(Math.random() * imageUrls.length)];
-const imageBuffer = await (await fetch(selectedImage)).buffer();
-const media = await prepareWAMessageMedia({ image: imageBuffer }, { upload: conn.waUploadToServer });
+
 
 await conn.sendMessage(m.chat, {
-  image: imageBuffer,
+  image: imageUrls,
   caption: menuText,
   contextInfo: {
     mentionedJid: [m.sender],
