f550fb0a40b04fed44922e422c463e6f8245e3a0
Deylin <deylinbo91@gmail.com>
2025-07-15 20:07:33 -0600

    Actualizar prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -1,7 +1,12 @@
 let handler = async (m, { conn }) => {
-  const h = global.subbotBanners
+  global.subbotBanners = global.subbotBanners || {};
+  global.defaultBanner = global.defaultBanner || 'https://raw.githubusercontent.com/Deylin-Eliac/kirito-bot-MD/main/src/catalogo.jpg';
 
+ 
+  const banner = global.subbotBanners[m.sender] || global.defaultBanner;
 
+  await conn.sendFile(m.chat, banner, 'canal.jpg', '📢 Este es el banner actual de tu subbot.', m);
+};
 
 handler.help = ['canal'];
 handler.tags = ['info'];
