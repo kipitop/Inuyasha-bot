5d66dac4166d5281230e20e5dc2aeb136f7291c7
Deylin <deylinbo91@gmail.com>
2025-07-16 00:52:36 -0600

    Actualizar sticker-pfp.js

--- plugins/sticker-pfp.js
+++ plugins/sticker-pfp.js
@@ -1,15 +1,32 @@
 /* Código hecho por Destroy
  - https://github.com/The-King-Destroy
+ - Modificado por Deylin
 */
 
-let handler = async (m, { conn }) => {
-    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-    let name = conn.getName(who);
-    let pp = await conn.profilePictureUrl(who, 'image').catch(() => 'https://files.catbox.moe/xr2m6u.jpg');
-    await conn.sendFile(m.chat, pp, 'profile.jpg', `*Foto de perfil de:»* ${name}`, m);
-}
+let handler = async (m, { conn, args, text }) => {
+    let who;
 
-handler.help = ['pfp @user'];
+    if (m.quoted) {
+        who = m.quoted.sender;
+    } else if (m.mentionedJid && m.mentionedJid[0]) {
+        who = m.mentionedJid[0];
+    } else if (text && text.replace(/\D/g, '').length > 4) {
+        let number = text.replace(/\D/g, '') + '@s.whatsapp.net';
+        who = number;
+    } else {
+        who = m.fromMe ? conn.user.jid : m.sender;
+    }
+
+    try {
+        let name = await conn.getName(who);
+        let pp = await conn.profilePictureUrl(who, 'image').catch(() => 'https://files.catbox.moe/xr2m6u.jpg');
+        await conn.sendFile(m.chat, pp, 'profile.jpg', `*Foto de perfil de: »* ${name}`, m);
+    } catch (e) {
+        await m.reply('❌ No se pudo obtener la foto de perfil.');
+    }
+};
+
+handler.help = ['pfp @user', 'pfp +numero'];
 handler.tags = ['sticker'];
 handler.command = ['pfp', 'getpic'];
 
