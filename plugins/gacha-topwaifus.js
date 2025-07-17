8bf6e38664d4939a6c340c82a2f07a81438c990d
Deylin <deylinbo91@gmail.com>
2025-07-16 22:16:03 -0600

    Actualizar gacha-topwaifus.js

--- plugins/gacha-topwaifus.js
+++ plugins/gacha-topwaifus.js
@@ -40,7 +40,7 @@ let handler = async (m, { conn, args }) => {
 
         await conn.reply(m.chat, message, m, fake);
     } catch (error) {
-        await conn.reply(m.chat, `✘ Error al cargar los personajes: ${error.message}`, m);
+        await conn.reply(m.chat, `✘ Error al cargar los personajes: ${error.message}`, m, fake);
     }
 };
 
