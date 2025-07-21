872718e61129bbf4309d87e9fb4fc85469ae02d4
Deylin <deylinbo91@gmail.com>
2025-07-20 20:37:39 -0600

    Actualizar _autodetect.js

--- plugins/_autodetect.js
+++ plugins/_autodetect.js
@@ -8,7 +8,7 @@ export async function before(m, { conn }) {
   if (!chat.detect) return;
 
   const usuario = `@${m.sender.split('@')[0]}`
-          const res = await fetch('https://files.catbox.moe/cstxnc.png'); // tu imagen personalizada
+          const res = await fetch('https://files.catbox.moe/258pqi.png'); // tu imagen personalizada
       const thumb = await res.buffer();
 
   const fkontak = {
