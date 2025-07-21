b14360a35a551cdc26ad8f270da4bea8729070d4
Deylin <deylinbo91@gmail.com>
2025-07-20 22:09:14 -0600

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,8 +1,11 @@
 const handler = async (m, { conn }) => {
-        let userId = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
-    const imageUrls = 'https://raw.githubusercontent.com/Deylin-Eliac/kirito-bot-MD/main/src/catalogo.jpg';
+  
 
-let info = `Hola @${userId.split('@')[0]} soy ${botname} y te invito a unirte a mis espacios oficiales:
+  let userId = m.mentionedJid?.[0] || m.sender;
+
+  const imageUrl = 'https://raw.githubusercontent.com/Deylin-Eliac/kirito-bot-MD/main/src/catalogo.jpg';
+
+  let info = `Hola @${userId.split('@')[0]} soy *${global.botname}* y te invito a unirte a mis espacios oficiales:
 
 𝗖𝗔𝗡𝗔𝗟:
 https://whatsapp.com/channel/0029VawF8fBBvvsktcInIz3m
@@ -11,16 +14,16 @@ https://whatsapp.com/channel/0029VawF8fBBvvsktcInIz3m
 http://bit.ly/3ImhCFl
 
 𝗖𝗢𝗡𝗧𝗔𝗖𝗧𝗢 𝗗𝗘𝗟 𝗖𝗥𝗘𝗔𝗗𝗢𝗥:
-https://wa.link/i3ytgw`
+https://wa.link/i3ytgw`;
 
-        await conn.sendMessage(m.chat, {
-  image: { url: imageUrls },
-  caption: info,
-contextInfo: {
-            mentionedJid: [userId] }
-}, { quoted: m });
+  await conn.sendMessage(m.chat, {
+    image: { url: imageUrl },
+    caption: info,
+    contextInfo: {
+      mentionedJid: [userId]
+    }
+  }, { quoted: m });
 };
-
 
 handler.help = ['grupos'];
 handler.tags = ['info'];
