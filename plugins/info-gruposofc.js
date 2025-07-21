3e5fd50b5ab46c9488d087ab7dcea299a58a3a81
Deylin <deylinbo91@gmail.com>
2025-07-20 22:04:28 -0600

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,10 +1,7 @@
-
-
 const handler = async (m, { conn }) => {
-  
+        let userId = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
 
-
-let info = `Hola soy ${botname} y te invito a unirte a mis espacios oficiales:
+let info = `Hola @${userId.split('@')[0]} soy ${botname} y te invito a unirte a mis espacios oficiales:
 
 𝗖𝗔𝗡𝗔𝗟:
 https://whatsapp.com/channel/0029VawF8fBBvvsktcInIz3m
@@ -15,6 +12,12 @@ http://bit.ly/3ImhCFl
 𝗖𝗢𝗡𝗧𝗔𝗖𝗧𝗢 𝗗𝗘𝗟 𝗖𝗥𝗘𝗔𝗗𝗢𝗥:
 https://wa.link/i3ytgw`
 
+        await conn.sendMessage(m.chat, {
+  image: { url: catalogo },
+  caption: info,
+contextInfo: {
+            mentionedJid: [userId] }
+}, { quoted: m });
 
 
 handler.help = ['grupos'];
