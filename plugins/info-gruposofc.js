1410deade12081ee619bc2fa0bcbae7c641940b3
Deylin <deylinbo91@gmail.com>
2025-07-20 22:02:15 -0600

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,69 +1,21 @@
-import { generateWAMessageFromContent, proto } from '@whiskeysockets/baileys'
+
 
 const handler = async (m, { conn }) => {
-  const texto = `✨ Pulsa el botón para unirte o visitar nuestros espacios oficiales`.trim()
+  
 
-  const buttons = [
-    {
-      name: 'cta_url',
-      buttonParamsJson: JSON.stringify({
-        display_text: ` ${emoji} Canal oficial`,
-        url: 'https://whatsapp.com/channel/0029VawF8fBBvvsktcInIz3m',
-        merchant_url: 'https://whatsapp.com/channel/0029VawF8fBBvvsktcInIz3m'
-      })
-    },
-    {
-      name: 'cta_url',
-      buttonParamsJson: JSON.stringify({
-        display_text: `${emoji} Grupo oficial`,
-        url: 'http://bit.ly/3ImhCFl',
-        merchant_url: 'http://bit.ly/3ImhCFl'
-      })
-    },
-    {
-      name: 'cta_url',
-      buttonParamsJson: JSON.stringify({
-        display_text: `${emoji} Creador`,
-        url: 'https://wa.link/i3ytgw',
-        merchant_url: 'https://wa.link/i3ytgw'
-      })
-    },
-    {
-      name: 'cta_url',
-      buttonParamsJson: JSON.stringify({
-        display_text: `${emoji} Regálanos una estrella en Github`,
-        url: 'https://github.com/Deylin-Eliac/kirito-bot-MD',
-        merchant_url: 'https://github.com/Deylin-Eliac/kirito-bot-MD'
-      })
-    }
-   ]
 
-  const messageContent = {
-    viewOnceMessage: {
-      message: {
-        messageContextInfo: {
-          deviceListMetadata: {},
-          deviceListMetadataVersion: 2
-        },
-        interactiveMessage: proto.Message.InteractiveMessage.create({
-          body: proto.Message.InteractiveMessage.Body.create({ text: texto }),
-          footer: proto.Message.InteractiveMessage.Footer.create({ text: ` ${botname} by Deylin` }),
-          header: proto.Message.InteractiveMessage.Header.create({ hasMediaAttachment: false }),
-          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
-            buttons
-          })
-        })
-      }
-    }
-  }
+let info = `Hola soy ${botname} y te invito a unirte a mis espacios oficiales:
 
-  const msg = generateWAMessageFromContent(m.chat, messageContent, {
-    userJid: m.sender,
-    quoted: m
-  })
+𝗖𝗔𝗡𝗔𝗟:
+https://whatsapp.com/channel/0029VawF8fBBvvsktcInIz3m
 
-  await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
-}
+𝗚𝗥𝗨𝗣𝗢 𝗢𝗙𝗖:
+http://bit.ly/3ImhCFl
+
+𝗖𝗢𝗡𝗧𝗔𝗖𝗧𝗢 𝗗𝗘𝗟 𝗖𝗥𝗘𝗔𝗗𝗢𝗥:
+https://wa.link/i3ytgw`
+
+
 
 handler.help = ['grupos'];
 handler.tags = ['info'];
