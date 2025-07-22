d03d95b3ff2ee14c17bf94f418f381bd21785de3
Deylin <deylinbo91@gmail.com>
2025-07-21 22:15:47 -0600

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -5,7 +5,7 @@ async function handler(m, { conn }) {
   const numCreador = '50433191934';
   const ownerJid = numCreador + '@s.whatsapp.net';
 
-    const res = await fetch('https://files.catbox.moe/w158ox.jpg);
+    const res = await fetch('https://files.catbox.moe/w158ox.jpg');
 const thumb2 = Buffer.from(await res.arrayBuffer());
 
   const fkontak = {
