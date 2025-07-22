8a598b40d335b11143e76078e0a101afc501b2e7
Deylin <deylinbo91@gmail.com>
2025-07-22 06:17:02 -0600

    Actualizar rpg-gremio.js

--- plugins/rpg-gremio.js
+++ plugins/rpg-gremio.js
@@ -50,7 +50,7 @@ let handler = async (m, { conn }) => {
     users[senderId].health -= evento.health;
   }
 
-  let img = 'https://qu.ax/bbfSN.jpg';
+  let img = 'https://files.catbox.moe/09r00j.jpg';
   let info = `╭━〔 Gremio de Aventureros 〕\n` +
              `┃Misión: *${evento.nombre}*\n` +
              `┃Evento: ${evento.mensaje}\n` +
