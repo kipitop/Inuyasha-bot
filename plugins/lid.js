9080644fd3f94f28d03194bf4740dc531c7dad5c
Deylin <deylinbo91@gmail.com>
2025-07-22 10:15:51 -0600

    Actualizar lid.js

--- plugins/lid.js
+++ plugins/lid.js
@@ -45,5 +45,6 @@ let handler = async function (m, { conn, groupMetadata }) {
 handler.command = ['lid']
 handler.help = ['lid']
 handler.tags = ['group']
+handler.admin = true
 
 export default handler
