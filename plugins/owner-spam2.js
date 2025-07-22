73b97f032338f4bd405057bfa113583202c7898d
Deylin <deylinbo91@gmail.com>
2025-07-22 05:23:26 -0600

    Actualizar owner-spam2.js

--- plugins/owner-spam2.js
+++ plugins/owner-spam2.js
@@ -46,7 +46,7 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 
     for (let i = 0; i < count; i++) {
       await conn.sendMessage(jid, {
-        text: text,
+        text: fancyMsg,
         contextInfo: {
           externalAdReply: {
             title: '🔥 Grupo Atacado',
