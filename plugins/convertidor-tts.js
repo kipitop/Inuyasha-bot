070e31b50d3b4debe8edd9a938c94fc72501e579
Deylin <deylinbo91@gmail.com>
2025-07-22 21:22:15 -0600

    Actualizar convertidor-tts.js

--- plugins/convertidor-tts.js
+++ plugins/convertidor-tts.js
@@ -1,41 +1,50 @@
 import gtts from 'node-gtts';
-import {readFileSync, unlinkSync} from 'fs';
-import {join} from 'path';
+import { readFileSync, unlinkSync } from 'fs';
+import { join } from 'path';
+
 const defaultLang = 'es';
-const handler = async (m, {conn, args, usedPrefix, command}) => {
+
+const handler = async (m, { conn, args, usedPrefix, command }) => {
   let lang = args[0];
   let text = args.slice(1).join(' ');
   if ((args[0] || '').length !== 2) {
     lang = defaultLang;
     text = args.join(' ');
   }
+
   if (!text && m.quoted?.text) text = m.quoted.text;
+
+  
+  text = text.replace(/[^\p{L}\p{N}\p{Zs}]/gu, '');
+
   let res;
   try {
     res = await tts(text, lang);
   } catch (e) {
     m.reply(e + '');
-    text = args.join(' ');
-    if (!text) throw `${emoji} Por favor, ingresé una frase.`;
+    text = args.join(' ').replace(/[^\p{L}\p{N}\p{Zs}]/gu, '');
+    if (!text) throw '❗ Por favor, ingresa una frase válida.';
     res = await tts(text, defaultLang);
   } finally {
     if (res) conn.sendFile(m.chat, res, 'tts.opus', null, m, true);
   }
 };
-handler.help = ['tts <lang> <teks>'];
+
+handler.help = ['tts <lang> <texto>'];
 handler.tags = ['transformador'];
-handler.group = true;
-handler.register = true
 handler.command = ['tts'];
+handler.group = true;
+handler.register = true;
 
 export default handler;
+
 
 function tts(text, lang = 'es') {
   console.log(lang, text);
   return new Promise((resolve, reject) => {
     try {
       const tts = gtts(lang);
-      const filePath = join(global.__dirname(import.meta.url), '../tmp', (1 * new Date) + '.wav');
+      const filePath = join(global.__dirname(import.meta.url), '../tmp', Date.now() + '.wav');
       tts.save(filePath, text, () => {
         resolve(readFileSync(filePath));
         unlinkSync(filePath);
