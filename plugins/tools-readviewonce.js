f6fec7c05e607ed44cd3e7709d21c145ab2611e6
Deylin <deylinbo91@gmail.com>
2025-07-22 22:28:40 -0600

    Actualizar tools-readviewonce.js

--- plugins/tools-readviewonce.js
+++ plugins/tools-readviewonce.js
@@ -1,13 +1,32 @@
 let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
 
 let handler = async (m, { conn }) => {
+    const res = await fetch('https://files.catbox.moe/69o0eq.jpg');
+const thumb2 = Buffer.from(await res.arrayBuffer());
+
+  const fkontak = {
+    key: {
+      participants: "0@s.whatsapp.net",
+      remoteJid: "status@broadcast",
+      fromMe: false,
+      id: "Halo"
+    },
+    message: {
+      locationMessage: {
+        name: '𝗦𝗘 𝗖𝗔𝗠𝗕𝗜𝗢́ 𝗔 𝗖𝗢𝗡𝗧𝗘𝗡𝗜𝗗𝗢 𝗣𝗨́𝗕𝗟𝗜𝗖𝗢',
+        jpegThumbnail: thumb2
+      }
+    },
+    participant: "0@s.whatsapp.net"
+  };
+
 if (!m.quoted) return conn.reply(m.chat, `👑 Responde a una imagen ViewOnce.`, m)
 if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `👑 Responde a una imagen ViewOnce.`, m)
 let buffer = await m.quoted.download(false);
 if (/videoMessage/.test(m.quoted.mtype)) {
-return conn.sendFile(m.chat, buffer, 'media.mp4', m.quoted.caption || '', m)
+return conn.sendFile(m.chat, buffer, 'media.mp4', m.quoted.caption || '', fkontak)
 } else if (/imageMessage/.test(m.quoted.mtype)) {
-return conn.sendFile(m.chat, buffer, 'media.jpg', m.quoted?.caption || '', m)
+return conn.sendFile(m.chat, buffer, 'media.jpg', m.quoted?.caption || '', fkontak)
 }}
 handler.help = ['ver']
 handler.tags = ['tools']
